4cbe0f1f4ec159a7ac70bc34e8b16c0f2bfecd0b
matteopariset <matteopariset@gmail.com>
2020-03-29 00:05:30 +0100

    Reorganize code

--- tz/cmd/core/main.go
+++ tz/cmd/core/main.go
@@ -11,174 +11,6 @@ import (
 	"../u"
 )
 
-// ToProvider specifies the link with a provider
-const ToProvider int = 1
-
-// ToPeer specifies the link with a peer
-const ToPeer int = 0
-
-// ToCustomer specifies the link with a customer
-const ToCustomer int = (-1)
-
-// Link represents and edge in AS graph
-type Link []int
-
-func (l Link) String() string {
-	var sb strings.Builder
-	for i := 0; i < len(l); i++ {
-		sb.WriteString("-> " + u.Str(l[i]))
-	}
-	return sb.String()
-}
-
-// Rel represents the type of edge in AS graph
-type Rel []int
-
-func (r Rel) String() string {
-	var sb strings.Builder
-	for i := 0; i < len(r); i++ {
-		switch r[i] {
-		case 1:
-			sb.WriteString("provider ")
-		case 0:
-			sb.WriteString("peer ")
-		case -1:
-			sb.WriteString("customer ")
-		}
-	}
-	return sb.String()
-}
-
-// Node epresents an AS in the graph
-type Node struct {
-	Asn   int
-	Links Link
-	Type  Rel
-}
-
-// ToNode initializes a Node, copying the arrays
-func ToNode(asn int, links Link, types Rel) Node {
-	var temp Node = Node{Asn: asn, Links: make(Link, len(links)), Type: make(Rel, len(types))}
-	copy(temp.Links, links)
-	copy(temp.Type, types)
-	return temp
-}
-
-func (n Node) String() string {
-	var sb strings.Builder
-	sb.WriteString("AS " + u.Str(n.Asn) + ": (" + u.Str(len(n.Links)) + " links)\n")
-	return sb.String()
-}
-
-// Speaker represents a (BGP, TZ, ...) speaker
-type Speaker struct {
-	Fresh        []bool
-	Destinations []*Node
-	NextHop      []*Node
-	Length       []int
-}
-
-// Unicode alternative symbols: '🡕''🡒''🡖'
-func linkTypeToSymbol(linkType int) string {
-	if linkType == 1 {
-		return `^`
-	} else if linkType == 0 {
-		return `=`
-	} else if linkType == -1 {
-		return `v`
-	} else {
-		return "?"
-	}
-}
-
-func (s *Speaker) String(n *Node) string {
-	// TODO: Refine it
-	var sb strings.Builder
-	for idx, dest := range s.Destinations {
-		sb.WriteString("	")
-		nextHopType := n.getNextHopType(s, idx)
-
-		sb.WriteString("(")
-		sb.WriteString(u.Str(s.Length[idx]))
-		sb.WriteString(") ")
-
-		switch s.Length[idx] {
-		case 0:
-		case 1:
-			if dest.Asn != s.NextHop[idx].Asn {
-				panic("Destination != NextHop in length=1 paths")
-			}
-			sb.WriteString(linkTypeToSymbol(nextHopType))
-			sb.WriteString(" ")
-		case 2:
-			sb.WriteString(linkTypeToSymbol(nextHopType))
-			sb.WriteString(" ")
-			sb.WriteString(u.Str(s.NextHop[idx].Asn))
-			sb.WriteString(" > ")
-		default:
-			sb.WriteString(linkTypeToSymbol(nextHopType))
-			sb.WriteString(" ")
-			sb.WriteString(u.Str(s.NextHop[idx].Asn))
-			sb.WriteString(" > ")
-			sb.WriteString("...")
-			sb.WriteString(" > ")
-		}
-
-		sb.WriteString(u.Str(dest.Asn))
-		sb.WriteString("\n")
-	}
-	return sb.String()
-}
-
-// Graph represents the AS graph
-type Graph struct {
-	Nodes     map[int]*Node
-	Speakers  map[int]*Speaker
-	unstable  map[*Node]bool
-	remaining int
-}
-
-func (l *Link) search(target int) int {
-	slice := (*l)[:]
-
-	var global int = 0
-
-	for {
-		if len(slice) == 0 {
-			panic("Element " + u.Str(target) + " not found in " + (*l)[:].String())
-		}
-		if len(slice) == 1 && slice[0] != target {
-			panic("Element " + u.Str(target) + " not found in " + (*l)[:].String())
-		}
-
-		cursor := len(slice) / 2
-
-		switch x := slice[cursor]; {
-		case x == target:
-			return global + cursor
-		case x < target:
-			slice = slice[cursor+1:]
-			global += cursor + 1
-		case x > target:
-			slice = slice[:cursor]
-		}
-	}
-}
-
-// This function assumes, for performance reasons, that the node n does NOT appear in path
-func (n *Node) getNextHopType(s *Speaker, destIndex int) int {
-	if s.Length[destIndex] == 0 {
-		// The node *n is the origin, return ToCustomer
-		return ToCustomer
-	}
-	return n.getNeighborType(s.NextHop[destIndex])
-}
-
-func (n *Node) getNeighborType(neighborNode *Node) int {
-	linkIndex := n.Links.search(neighborNode.Asn)
-	return n.Type[linkIndex]
-}
-
 // LoadFromCsv imports the structure of the AS graph from a preprocessed .csv file
 func LoadFromCsv(filename string) (Graph, error) {
 
@@ -232,141 +64,6 @@ func LoadFromCsv(filename string) (Graph, error) {
 
 	return Graph{Nodes: nodes, Speakers: speakers, unstable: unstableSet, remaining: remaining}, nil
 
-}
-
-// InitSpeaker initializes the Speaker associated with a certain Node
-func InitSpeaker(node *Node) *Speaker {
-	// TODO: Clean method
-	speaker := Speaker{Fresh: nil, Destinations: nil, NextHop: nil, Length: nil}
-	return &speaker
-}
-
-// SetDestinations updates the speakers according to the set of chosen destinations
-func (g *Graph) SetDestinations(dest map[int]bool) {
-	for asn := range dest {
-		if g.Speakers[asn].hasRoute(g.Nodes[asn]) < 0 {
-			g.Speakers[asn].Fresh = append(g.Speakers[asn].Fresh, true)
-			g.Speakers[asn].Destinations = append(g.Speakers[asn].Destinations, g.Nodes[asn])
-			g.Speakers[asn].NextHop = append(g.Speakers[asn].NextHop, g.Nodes[asn])
-			g.Speakers[asn].Length = append(g.Speakers[asn].Length, 0)
-			g.setUnstable(g.Nodes[asn])
-		}
-	}
-}
-
-func (s *Speaker) hasRoute(dest *Node) int {
-	for i, d := range s.Destinations {
-		if d == dest {
-			return i
-		}
-	}
-	return -1
-}
-
-func (s *Speaker) heardFrom(destIndex int, neighbor *Node) bool {
-	return s.NextHop[destIndex].Asn == neighbor.Asn
-}
-
-func (s *Speaker) addDestination(currNode *Node, dest *Node, nextHop *Node, length int) bool {
-	s.Fresh = append(s.Fresh, true)
-	s.Destinations = append(s.Destinations, dest)
-	s.NextHop = append(s.NextHop, nextHop)
-	s.Length = append(s.Length, length+1)
-	return true
-}
-
-func (s *Speaker) updateDestination(currNode *Node, routeIndex int, dest *Node, nextHop *Node, length int) bool {
-	// Compare with current path
-	oldNextType := currNode.getNextHopType(s, routeIndex)
-	newNextType := currNode.getNeighborType(nextHop)
-
-	// Applies: customer < peer < provider (smaller is better)
-	if (newNextType < oldNextType) ||
-		(oldNextType == newNextType && length+1 < s.Length[routeIndex]) {
-		// Higher preference or same preference, but the new route is shorter
-		s.Fresh[routeIndex] = true
-		s.Length[routeIndex] = length + 1
-		s.NextHop[routeIndex] = nextHop
-		return true
-	}
-
-	return false
-}
-
-func (s *Speaker) advertise(neighborNode *Node, destination *Node, nextHop *Node, length int) bool {
-	routeNum := s.hasRoute(destination)
-	if routeNum < 0 {
-		// The neighbor speaker does not have the destination yet
-		return s.addDestination(neighborNode, destination, nextHop, length)
-	}
-	// The neighbor has the destination, check which one is better
-	return s.updateDestination(neighborNode, routeNum, destination, nextHop, length)
-}
-
-func (g *Graph) setUnstable(node *Node) {
-	_, pr := g.unstable[node]
-	if !pr {
-		g.remaining++
-		g.unstable[node] = true
-	}
-}
-
-func (g *Graph) setStable(node *Node) {
-	_, pr := g.unstable[node]
-	if pr {
-		g.remaining--
-		delete(g.unstable, node)
-	}
-}
-
-// Activate evolves the status of a speaker
-func (g *Graph) Activate(nodeIndex int) int {
-
-	nd := g.Nodes[nodeIndex]
-	sp := g.Speakers[nodeIndex]
-
-	g.setStable(nd)
-
-	var messagesSent int
-
-	for i := 0; i < len(sp.Fresh); i++ {
-		if sp.Fresh[i] {
-			nextHopType := nd.getNextHopType(sp, i)
-			// Advertise change to neighbors
-			for k, link := range nd.Links {
-
-				// Check that it's not this neighbor that has advertised this route to me
-				if !sp.heardFrom(i, g.Nodes[link]) {
-					var flag bool = false
-
-					switch linkType := nd.Type[k]; {
-					// CUSTOMER: Advertise all routes
-					case linkType == ToCustomer:
-						flag = g.Speakers[link].advertise(g.Nodes[link], sp.Destinations[i], nd, sp.Length[i])
-						messagesSent++
-
-					// PEER: Advertise routes from customers and peers? (TODO?)
-					case linkType == ToPeer && (nextHopType == ToCustomer || nextHopType == ToPeer):
-						flag = g.Speakers[link].advertise(g.Nodes[link], sp.Destinations[i], nd, sp.Length[i])
-						messagesSent++
-
-					// PROVIDER: Advertise routes from customers
-					case linkType == ToProvider && nextHopType == ToCustomer:
-						flag = g.Speakers[link].advertise(g.Nodes[link], sp.Destinations[i], nd, sp.Length[i])
-						messagesSent++
-					}
-
-					if flag {
-						g.setUnstable(g.Nodes[link])
-					}
-				}
-
-			}
-			sp.Fresh[i] = false
-		}
-	}
-
-	return messagesSent
 }
 
 var sh *Shell
@@ -428,102 +125,8 @@ func main() {
 		panic(err)
 	}
 
-	/*
-		graph.SetDestinations(map[int]bool{
-			398255: true,
-			12637:  true,
-			38191:  true,
-		})
-
-		fmt.Printf("Activate node #398255:\n")
-		graph.Activate(398255)
-		graph.printRoutes(398255)
-		graph.printRoutes(174)
-		graph.printRoutes(3356)
-		graph.printRoutes(4)
-
-		fmt.Printf("Activate node #174:\n")
-		graph.Activate(174)
-		graph.printRoutes(398255)
-		graph.printRoutes(174)
-		graph.printRoutes(3356)
-		graph.printRoutes(4)
-
-		fmt.Printf("Activate node #4:\n")
-		graph.Activate(4)
-		graph.printRoutes(398255)
-		graph.printRoutes(174)
-		graph.printRoutes(3356)
-		graph.printRoutes(4)
-
-		fmt.Printf("Activate node #174:\n")
-		graph.Activate(174)
-		graph.printRoutes(398255)
-		graph.printRoutes(174)
-		graph.printRoutes(3356)
-		graph.printRoutes(4)
-
-		fmt.Println("Remaining: " + u.Str(graph.remaining))
-
-		idx := graph.Nodes[3356].Links.search(174)
-		fmt.Println("nodes[" + u.Str(idx) + "] = " + u.Str(graph.Nodes[3356].Links[idx]))
-		fmt.Println(graph.Nodes[3356].Type[idx])
-
-		//graph.Evolve()
-
-	*/
 	for graph.ExecCommand() {
 	}
-}
-
-// Evolve updates the system until its convergence
-func (g *Graph) Evolve() (stepsToConvergence int) {
-	stepsToConvergence = 0
-
-	var roundNum int = 0
-	for g.remaining > 0 {
-		fmt.Printf("Round %d : %d activation queued\n", roundNum, g.remaining)
-
-		for k := range g.unstable {
-			sh.Overwrite("	Activating AS#", Green, u.Str(k.Asn), Clear)
-			stepsToConvergence += g.Activate(k.Asn)
-		}
-		fmt.Print("\n")
-		roundNum++
-	}
-
-	return
-}
-
-func (g *Graph) validateAsn(asn int) bool {
-	_, ok := g.Nodes[asn]
-	return ok
-}
-
-// GetRoute returns a path (if it exists) from an origin to a destination along with the types of links used
-// The first array is 1 ELEMENT LONGER than the second
-func (g *Graph) GetRoute(originAsn int, destinationAsn int) ([]*Node, []int) {
-	if !g.validateAsn(originAsn) || !g.validateAsn(destinationAsn) {
-		fmt.Println("UNKNOWN ROUTE")
-		return nil, nil
-	}
-	route := make([]*Node, 0, 5)
-	linkTypes := make([]int, 0, 5)
-
-	cursorAsn := originAsn
-	for cursorAsn != destinationAsn {
-		routeNum := g.Speakers[cursorAsn].hasRoute(g.Nodes[destinationAsn])
-		if routeNum < 0 {
-			return nil, nil
-		}
-		route = append(route, g.Nodes[cursorAsn])
-		hopType := g.Nodes[cursorAsn].getNextHopType(g.Speakers[cursorAsn], routeNum)
-		linkTypes = append(linkTypes, hopType)
-		cursorAsn = g.Speakers[cursorAsn].NextHop[routeNum].Asn
-	}
-
-	// Add destination AS node
-	return append(route, g.Nodes[destinationAsn]), linkTypes
 }
 
 // PrintRoute nicely prints the route from origin to destination returned by GetRoute
@@ -544,13 +147,21 @@ func (g *Graph) PrintRoute(originAsn int, destinationAsn int) {
 	}
 }
 
-func (g *Graph) printSpeakerStatus(asn int) {
-	if !g.validateAsn(asn) {
-		fmt.Println("INVALID AS number")
-	} else {
-		fmt.Println("Speaker #" + u.Str(g.Nodes[asn].Asn))
-		fmt.Println("	neighbors: " + u.Str(len(g.Nodes[asn].Links)))
-		fmt.Println("	ROUTES: ")
-		fmt.Println(g.Speakers[asn].String(g.Nodes[asn]))
+// Evolve updates the system until its convergence
+func (g *Graph) Evolve() (stepsToConvergence int) {
+	stepsToConvergence = 0
+
+	var roundNum int = 0
+	for g.remaining > 0 {
+		fmt.Printf("Round %d : %d activation queued\n", roundNum, g.remaining)
+
+		for k := range g.unstable {
+			sh.Overwrite("	Activating AS#", Green, u.Str(k.Asn), Clear)
+			stepsToConvergence += g.Activate(k.Asn)
+		}
+		fmt.Print("\n")
+		roundNum++
 	}
+
+	return
 }
